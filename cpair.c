a333b4cc7823b98df4de88e4bf27843bb6f9cea4
João Valença <jsvalenca@gmail.com>
2013-10-01 19:05:41 +0000

    closest pair done, can be improved

--- cpair.c
+++ cpair.c
@@ -2,65 +2,69 @@
 #include<stdlib.h>
 #include<math.h>
 
-#define MAX 10000
+#define MAX 100000
 #define min(a,b) ((a)<(b))?(a):(b)
+
+double dist(double a[2],double b[2]);
+int comp(double *a, double *b);
+double divc(double v[MAX][2],int s,int f,int l);
 
 double points[MAX][2];
 double min_d;
 int N,D;
 int P1,P2;
 
-int comp(double *a, double *b);
-double divconq(int s,int f);
-double lrmin(int s,int f);
-double dist(double a[2],double b[2]);
-
 int main(){
-  int i=0;
+  int i=0,j;
+  double m=1000000;
   scanf("%d %d",&N,&D);
-  for(i=0;scanf("%lf %lf",&points[i][0],&points[i][1])!=-1;i++);
+  
+  for(i=0;i<N;i++)
+    for(j=0;j<D;j++)
+      scanf("%lf",&points[i][j]);
+  
 
   qsort(points,N,sizeof(double[2]),(int(*)(const void*,const void*))comp);
+  
+  for(i=0;i<N;i++){
+    for(j=i+1;j<N;j++){
+      m=min(m,dist(points[i],points[j]));
+    }
+  }
 
-  divconq(0,N);
+  printf("%f\n",m);
+  
+  printf("%f\n",divc(points,0,N,0));
 
-  printf("%f\n",min_d);
   return 0;
 }
 
-double divconq(int s, int f){
-  double m,r,l;
-  if(s<f-1){
-    l=divconq(s,f/2);
-    r=divconq(f/2,f);
-    
-    m=min(l,r);
-    
-    m=min(m,lrmin(s,f));
+double divc(double v[MAX][2],int s,int f,int l){
+  int i,j,k,d;
+  double m=100000;
+  if (f==s+3){
+    return min(dist(v[s],v[s+2]),min(dist(v[s],v[s+1]),dist(v[s+1],v[s+2])));
   }
-  return m;
-}
-
-double lrmin(int s,int f){
-  int i,j,k,m;
-  double tmp;
-  m=(f-s)/2;
-  for(i=s;i<m;i++){
-    k=0;
-    for(j=m;j<f;j++){
-      if(k==8 || points[j][0]-points[i][0]>=min_d) break;
-      if(abs(points[j][1]-points[i][1])>=min_d) continue;
-      else{
-	k++;
-	tmp=dist(points[i],points[j]);
-	if(tmp<min_d){
-	  min_d=tmp;
-	  P1=i;P2=j;
+  else if(f==s+2){
+    return dist(v[s],v[s+1]);
+  }
+  else{
+    d=s+(f-s)/2;
+    m=min(divc(v,s,d,l),divc(v,d,f,l+1));
+    
+    for(i=d-1;i>=s;i--){
+      k=0;
+      for(j=d;j<f;j++){
+	if(k==7  || v[j][0]-v[i][0]>=m) break;      
+	if(abs(v[j][1]-v[i][1])>=m) continue;
+	else{
+	  m=min(m,dist(v[i],v[j]));
+	  k++;
 	}
       }
     }
+    return m;
   }
-  return 0;
 }
 
 int comp(double *a, double *b){
@@ -75,4 +79,3 @@ int comp(double *a, double *b){
 double dist(double a[2],double b[2]){
   return sqrt(((a[0]-b[0])*(a[0]-b[0]))+((a[1]-b[1])*(a[1]-b[1])));
 }
-
